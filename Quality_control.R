738d551525f20e7d491c8a2d007b942b264297b5
leopoldguyot <leopold.guyot@student.uclouvain.be>
2023-11-15 17:54:17 +0100

    [FT] - First iteration of the Quality control exploration

---
+++ Quality_control.R
@@ -0,0 +1,57 @@
+############## IMPORTS #################
+library(ShortRead)
+library(ggplot2)
+############# FUNCTIONS ###############
+
+load_quality <- function(filename){
+  fq <- readFastq(filename)
+  return(quality(fq))
+}
+
+extract_read_means <- function(quality){
+  alphabetScore(quality(fq))/width(quality)
+}
+
+extract_cycle_means <- function(quality){
+  tabel <- alphabetByCycle(quality)
+  rownames(tabel) <- sapply(row.names(tabel),
+                            function(rowname) utf8ToInt(rowname)-33)
+  tabel <- as.data.frame(tabel)
+  colnames(tabel) <- 1:ncol(tabel)
+  Ncol <- colSums(tabel)
+  for (row in 1:nrow(tabel)) {
+    tabel[row,] <- tabel[row,] * as.integer(rownames(tabel[row,]))
+  }
+  i = 0
+  means <- apply(tabel, 2, function(col) {
+    i = i+1
+    sum(col)/Ncol[i]})
+  return(means)
+}
+
+
+read_mean_plot <- function(quality){
+  means <- extract_read_means(quality)
+  #Name
+  ggsave("Figures/legacy_plots/", ggplot(mapping = aes(x = means, after_math(scaled)))+
+    geom_density())
+}
+
+cycle_mean_plot <- function(quality){
+  means <- extract_cycle_means(quality)
+  means <- as.data.frame(test)
+  ggsave("Figures/legacy_plots/",ggplot(means, aes(x = 1:length(means), y = means))+geom_line)
+}
+run_qc_plots <- function(fastqfiles){
+  for (file in fastqfiles){
+    quality <- load_quality(file)
+    read_mean_plot(quality)
+    cycle_mean_plot(quality)
+  }
+
+}
+############### Main code #######################
+
+reads_files <- list.files(path = "data/mirnaseq_data", full.names = TRUE)
+
+run_qc_plots(reads_files)
